9e7272d7747733f08a62f28cd508a2c12bc6f4d8
Mel_v <Mvvento@gmail.com>
2024-05-04 13:18:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Define product data (replace with your actual product information)
+const products = [
+  {
+    id: 1,
+    name: "Moisturizing Face Cream",
+    image: "[image of face cream]", // Replace with actual image URL
+    price: 29.99,
+    description: "A luxurious cream that hydrates"
+  }]
